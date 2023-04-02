b9cc9411276b6a71fa25dd79312944a2c82487dd
Gabriel Oliveira <gabriel_lopeslomeu@hotmail.com>
2023-04-02 16:18:18 -0300

    versão finalizada 2 sem bônus

--- script.js
+++ script.js
@@ -69,3 +69,55 @@ function telaFinal(selector){
     const elemento = document.querySelector(selector);
     elemento.classList.remove('escondido');
 }
+
+
+function whats(checkFood){
+    
+    //seleção, soma e edição dos preços dos pratos:
+    const foodselect = document.querySelector('.pratoverde .preço');
+    const pricefood  = foodselect.innerHTML;
+    const pricefoodConvertido = pricefood.replace(',','.');
+    const preçofinalfood = parseFloat(pricefoodConvertido)
+    
+    //seleção dos nomes dos pratos:
+    const foodselectname = document.querySelector('.pratoverde .nome-prato');
+    const foodname = foodselectname.innerHTML + "%0A";
+    console.log(foodname)
+
+
+    //seleção, soma e edição dos preços das bebidas:
+    const bebidaselect = document.querySelector('.bebidaverde .preço');
+    const pricebebida = bebidaselect.innerHTML;
+    const pricebebidaConvertido = pricebebida.replace(',','.');
+    const preçofinalbebida = parseFloat(pricebebidaConvertido)
+
+    //seleção dos nomes das bebidas:
+    const bebidaselectname = document.querySelector('.bebidaverde .nome-prato');
+    const bebidaname = bebidaselectname.innerHTML + "%0A";
+    console.log(bebidaname);
+ 
+    //seleção, soma e edição dos preços dos doces:
+    const doceselect = document.querySelector('.doceverde .preço');
+    const pricedoce = doceselect.innerHTML;
+    const pricedoceConvertido = pricedoce.replace(',','.');
+    const preçofinaldoce = parseFloat(pricedoceConvertido)
+
+     //seleção dos nomes dos doces:
+     const doceselectname = document.querySelector('.doceverde .nome-prato');
+     const docename = doceselectname.innerHTML + "%0A";
+     console.log(docename);
+    
+
+    const pricetotal = preçofinalbebida + preçofinaldoce + preçofinalfood;
+    const pricetotaledit = pricetotal.toFixed(2).replace('.',',');
+    const string = 
+    "Ol%C3%A1%2C%20gostaria%20de%20fazer%20o%20pedido%3A%0A" + "-%20Prato%3A%20" + foodname + "-%20Bebida%3A%20" + bebidaname + "-%20Sobremesa%3A%20" + docename + "Total%3A%20R%24%20" + pricetotaledit;
+    
+    const msn = document.querySelector('.wpp');
+    const evento = msn.setAttribute("href","https://wa.me/5524999378096?text="+string)
+    
+}
+
+
+
+
